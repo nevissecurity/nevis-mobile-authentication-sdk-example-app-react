af1aff6322ddd85f8e87027ff6997c4895e6abbc
Tamas Toth <tamas.toth@nevis-security.com>
2024-03-20 10:32:19 +0100

    NEVISACCESSAPP-5628: Add Biometric/fingerprint authentication confirmation and cancellation
    - A new Confirmation screen added where the user is able to confirm/start or cancel either the biometric, fingerprint or device passcode verification.
    - Modified `BiometricUserVerifierImpl`, `FingerprintUserVerifierImpl` and `DevicePasscodeUserVerifierImpl` classes to navigate to the Confirmation screen instead of starting the verification automatically.

--- src/userInteraction/DevicePasscodeUserVerifierImpl.ts
+++ src/userInteraction/DevicePasscodeUserVerifierImpl.ts
@@ -3,30 +3,24 @@
  */
 
 import {
-	DevicePasscodePromptOptions,
 	DevicePasscodeUserVerificationContext,
 	DevicePasscodeUserVerificationHandler,
 	DevicePasscodeUserVerifier,
 } from '@nevis-security/nevis-mobile-authentication-sdk-react';
-import i18next from 'i18next';
 
-import { ErrorHandler } from '../error/ErrorHandler';
-import { OperationType } from '../model/OperationType';
+import { AuthenticatorUtils } from '../utility/AuthenticatorUtils';
+import * as RootNavigation from '../utility/RootNavigation';
 
 export class DevicePasscodeUserVerifierImpl extends DevicePasscodeUserVerifier {
 	async verifyDevicePasscode(
-		_context: DevicePasscodeUserVerificationContext,
+		context: DevicePasscodeUserVerificationContext,
 		handler: DevicePasscodeUserVerificationHandler
 	): Promise<void> {
 		console.log('Please start device passcode user verification.');
-		await handler
-			.listenForOsCredentials(
-				DevicePasscodePromptOptions.create(
-					i18next.t('devicePasscode.popup.title'),
-					i18next.t('devicePasscode.popup.description')
-				)
-			)
-			.catch(ErrorHandler.handle.bind(null, OperationType.unknown));
+		RootNavigation.navigate('Confirmation', {
+			authenticator: AuthenticatorUtils.localizedTitle(context.authenticator),
+			handler: handler,
+		});
 	}
 
 	onValidCredentialsProvided(): void {
