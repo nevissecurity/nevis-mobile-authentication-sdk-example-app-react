e0bb7c03b7f03181926c720a722f2fa79aa50cb1
Dániel Tóth <daniel.toth@nevis.net>
2024-12-11 10:32:26 +0100

    NEVISACCESSAPP-6294: upgrade to rn 0.78.0-rc.2

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,19 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 // Path to local nevis-mobile-authentication-sdk-react package
 //const nevisMobileAuthenticationSdkPackagePath = '../nevis-mobile-authentication-sdk-react';
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
-    /*resolver: {
+	/*resolver: {
         nodeModulesPaths: [nevisMobileAuthenticationSdkPackagePath],
     },
     watchFolders: [nevisMobileAuthenticationSdkPackagePath],*/
 };
-
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
