feacd8bd7a47da5973aebc323af7371377843326
Tamas Toth <tamas.toth@nevis.net>
2026-02-27 11:19:27 +0100

    NEVISACCESSAPP-7255: Update to React Native 0.81.6
    - Project has been upgraded to use React Native 0.81.6
    - Using precompiled React Native builds for iOS

--- ios/nevis_mobile_authentication_sdk_example_app_react/AppDelegate.swift
+++ ios/nevis_mobile_authentication_sdk_example_app_react/AppDelegate.swift
@@ -5,29 +5,11 @@
 import Foundation
 
 @main
-class AppDelegate: RCTAppDelegate {
-
-	// MARK: RCTAppDelegate Overrides
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	// MARK: UIApplicationDelegate
 
 	// swiftformat:disable:next unusedArguments
-	override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		moduleName = "nevis-mobile-authentication-sdk-example-app-react"
-		// Prevent RCTAppDelegate from creating the window, will be created in SceneDelegate.
-		automaticallyLoadReactNativeWindow = false
-		return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-	}
-
-	override func bundleURL() -> URL? {
-		#if DEBUG
-			return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-		#else
-			return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-		#endif
-	}
-
-	// MARK: RCTBridgeDelegate Overrides
-
-	override func sourceURL(for bridge: RCTBridge) -> URL? {
-		bridge.bundleURL ?? bundleURL()
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		true
 	}
 }
